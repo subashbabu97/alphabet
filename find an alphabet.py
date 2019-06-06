2f2b15de968fff4d9352faa05c6f1374315948b7
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-06-06 09:51:58 +0530

    Update find an alphabet.py

--- find an alphabet.py
+++ find an alphabet.py
@@ -1,6 +1,6 @@
 ch_str=input("enter an character :")
 if ((ch_str>='a' and ch_str<='z') or (ch_str>='A' and ch_str<='Z')):
-        print(ch_str,"is an alphabet")
+        print(" alphabet")
 else:
-        print(ch_str,"is not an alphabet")
+        print("NO")
         
